482aee63751ba9c442b3050c536b1878d5d747f9
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-10 12:29:24 -0500

    basic working app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import avengersData from './Components/avengerData';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {avengersData};
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Avengers: by Emily</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.avengersData.map(avengers => (<div key = {avengers.id}>{avengers.name}</div>))}
         </p>
       </div>
     );
---
+++ src/Components/avengerData.js
@@ -0,0 +1,12 @@
+const avengersData = [
+    {id:1,name:'Thor'}, 
+    {id:2, name:'Loki'}, 
+    {id:3,name:'Ironman'}, 
+    {id:4, name:'Captain America'}, 
+    {id:5, name:'Hulk'}, 
+    {id:6, name:'Black Widow'}, 
+    {id:7, name:'Hawkeye'}, 
+    {id:8, name:'Scarlet Witch'}
+  ]
+
+  export default avengersData;
